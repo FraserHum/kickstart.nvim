8134df60d3e5bad0e1a812ac129e80acb24b5c54
Fraser Humphries <fraser.n.humphries@gmail.com>
2026-04-07 08:53:49 +1200

    diff view

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -2,4 +2,12 @@ return {
   "sindrets/diffview.nvim",
   event = "VeryLazy",
   dependencies = { "nvim-lua/plenary.nvim" },
+  opts = {
+    view = {
+      merge_tool = {
+        layout = "diff4_mixed",
+        disable_diagnostics = true,
+      },
+    },
+  },
 }
